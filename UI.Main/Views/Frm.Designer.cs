ac548539b40426d0f62c7fca5e5daae6225047df
Adilson72 <jochico7@gmail.com>
2021-05-06 18:53:54 -0300

    texo para Jose

--- UI.Main/Views/Frm.Designer.cs
+++ UI.Main/Views/Frm.Designer.cs
@@ -38,17 +38,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(35, 135);
+            this.button1.Location = new System.Drawing.Point(94, 108);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Jose";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 109);
+            this.textBox1.Location = new System.Drawing.Point(79, 81);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 1;
